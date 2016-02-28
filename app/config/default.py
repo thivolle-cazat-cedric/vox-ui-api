10632422fbceafbd73121ead7a774b54a27980fb
thivolle cedric <cedric@thivolle-cazat.fr>
2016-02-28 11:32:07 +0100

    edit default configuration

--- app/config/default.py
+++ app/config/default.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+PORT = 6500
+PERMANENT_SESSION_LIFETIME = 3600 * 24 * 14
+
+SESSION_COOKIE_NAME = "_vua_s"
+
 BASE_URL = "https://api.voxity.fr/api/v1"
 AUTHORIZATION_BASE_URL = 'https://api.voxity.fr/api/v1/dialog/authorize'
-TOKEN_URL = 'https://api.voxity.fr/api/v1/oauth/token'
+TOKEN_URL = BASE_URL + '/oauth/token'
+
 CLIENT_ID = None
 CLIENT_SECRET = None
-PORT = 6500
 REDIRECT_URI = "http://127.0.0.1:6500/callback"
-
 SECRET_KEY = "azerty"
 DEBUG = True
-OAUTHLIB_INSECURE_TRANSPORT = '1'
 
-PERMANENT_SESSION_LIFETIME = 3600 * 24 * 14
+OAUTHLIB_INSECURE_TRANSPORT = '1'
