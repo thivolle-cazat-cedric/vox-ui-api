c16a1b9c59b3ce681a1a77628517e9f0a5d2680c
thivolle cedric <cedric@thivolle-cazat.fr>
2016-03-14 20:30:43 +0100

    improve filter number_clear (check if is number)

--- app/utils/jinja2_filters.py
+++ app/utils/jinja2_filters.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, unicode_literals
+import re
+
+EXTERNAL_PHONE_NUM_RE = re.compile('\+?\d{5,}')
 
 def number_clear(value, space=True):
     if value and len(value) > 3 and value[0:3] == "+33":
         value = '0' + value[3:]
 
-    if space:
+    if space and EXTERNAL_PHONE_NUM_RE.search(value):
         value = " ".join(value[i:i + 2] for i in range(0, len(value), 2))
 
     return value
