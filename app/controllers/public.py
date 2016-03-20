239ec1f94ef166fb4723871b9d9ce8bb7ec4b4b7
thivolle cedric <cedric@thivolle-cazat.fr>
2016-03-20 15:34:47 +0100

    add title description

--- app/controllers/public.py
+++ app/controllers/public.py
@@ -16,15 +16,16 @@ def index():
 def changelog():
     return render_template(
         'public/changelog.html',
-        changelog=markdown(render_template('changelog.md'))
+        changelog=markdown(render_template('changelog.md')),
+        page_title="changelog"
     )
 
 
 @PUBLIC.route('about.html', methods=["GET"])
 def about():
-    return render_template('public/about.html')
+    return render_template('public/about.html', page_title="à propos")
 
 
 @PUBLIC.route('features.html', methods=["GET"])
 def features():
-    return render_template('public/features.html')
+    return render_template('public/features.html', page_title="fonctionalités")
