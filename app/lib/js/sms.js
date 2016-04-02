cdf77e5d0a1f32fb6a0045a1e97c105f1f1c9445
thivolle cedric <cedric@thivolle-cazat.fr>
2016-04-02 16:30:05 +0200

    improve sms javascript

--- app/lib/js/sms.js
+++ app/lib/js/sms.js
@@ -1,7 +1,14 @@
 var MAX_CONTENT_LENGTH = 160;
 var  MOBILE_LIST = [];
 
+/**
+ * Retrun
+ * @param  {String} number the number associate in this tag
+ * @param  {String} name   *optional* - The name diaplay on tag (if undefined, search in MOBILE_LIST, if nothing match number is display)
+ * @return {object}        Dom object (span.label)
+ */
 function labelRemovable(number, name){
+
     var findeName = MOBILE_LIST.filter(function(obj) {return (obj.num == number);});
     if (name === undefined && !findeName) {
         name = number;
@@ -16,11 +23,16 @@ function labelRemovable(number, name){
         }
         if (loop) {name = number;}
     }
-    return '<span class="label label-default label-tel" data-telnum="'+number+'">'+ name +' <i class="fa fa-times-circle"></i></span>'
+    return $('<span>',{'class': 'label label-primary label-tel','data-telnum': number}).text(name).append($('<i>', {class:'fa fa-times-circle'}));
 }
 
-function init(d, status) {
-    var regTel = /^(\+33|0)[6-7]\d{8}$/; 
+/**
+ * init contact autocompletion
+ * @return {[type]} [description]
+ */
+function init() {
+    $('[data-target="#contactListModal"]').attr('disabled', 'disabled');
+    var mobileReg = /^(\+33|0)[6-7]\d{8}$/; 
     MOBILE_LIST = [];
     $.ajax({
         url: "/contacts/all.json",
@@ -28,72 +40,85 @@ function init(d, status) {
         success: function(d, status) {
             if (d.data) {
                 d.data.forEach(function(el) {
-                    if (regTel.test(el['telephoneNumber'])) {
+                    if (mobileReg.test(el['telephoneNumber'])) {
                         MOBILE_LIST.push({
                             name: el['cn'],
                             num: el['telephoneNumber']
                         });
                     }
-                    if (regTel.test(el['mobile'])) {
+                    if (mobileReg.test(el['mobile'])) {
                         MOBILE_LIST.push({
                             name: el['cn'],
                             num: el['mobile']
                         });
                     }
                 });
-                initLabelField()
-            }
-        }
-    });
-}
-function initLabelField(){
-    var contactSrc = new Bloodhound({
-        local: MOBILE_LIST,
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-    });
-    contactSrc.initialize();
+                var contactSrc = new Bloodhound({
+                    local: MOBILE_LIST,
+                    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+                    queryTokenizer: Bloodhound.tokenizers.whitespace,
+                });
+                contactSrc.initialize();
 
-    $('#tagsnum').tagsinput({
-        itemValue: 'num',
-        itemText: 'name',
-        freeInput: true,
-        typeaheadjs: {
-            freeInput: true,
-            displayKey: 'name',
-            source: contactSrc.ttAdapter(),
-            itemText: 'name',
-            itemValue: 'num',
+                $('#tagsnum').tagsinput({
+                    itemValue: 'num',
+                    itemText: 'name',
+                    freeInput: true,
+                    typeaheadjs: {
+                        freeInput: true,
+                        displayKey: 'name',
+                        source: contactSrc.ttAdapter(),
+                        itemText: 'name',
+                        itemValue: 'num',
+                    }
+                });
+                refreshLabelList();
+                $('#tagsnum').on('itemAdded', function(event) {
+                    if($('#destnum-label [data-telnum="'+event.item.num+'"]').length == 0){
+                        addNum(event.item.num, event.item.name);
+                    }
+                });
+                $('#tagsnum').on('itemRemoved', function(event) {try{ $('#destnum-label [data-telnum="'+event.item.num+'"] .fa').click()} catch(e){}});
+                $('#destnum-label .label').each(function(){
+                    var $this = $(this);
+                    if ($this.attr('data-telnum').trim() != $this.text().trim()) {
+                        $('#tagsnum').tagsinput('add', {
+                            num:$this.attr('data-telnum'),
+                            name: $this.text()
+                        });
+                    }
+                })
+                $('[data-target="#contactListModal"]').removeAttr('disabled');
+            } else {
+                toastr["warning"]("Réponse innatendu lors du chargement des contact", "Chargement des contacts")
+            }
+        },
+        error: function(){
+            toastr["warning"]("Réponse innatendu lors du chargement des contact", "Chargement des contacts")
         }
     });
-    $('#tagsnum').on('itemAdded', function(event) {addNum(event.item.num, event.item.name)});
-    $('#tagsnum').on('itemRemoved', function(event) {$('#destnum-label [data-telnum="'+event.item.num+'"] .fa').click()});
-    refreshLabelList();
-    $('#destnum-label .label').each(function(){
-        var $this = $(this);
-        if ($this.attr('data-telnum').trim() != $this.text().trim()) {
-            $('#tagsnum').tagsinput('add', {
-                num:$this.attr('data-telnum'),
-                name: $this.text()
-            });
-        }
-    })
-    refreshLabelList();
 }
 
 function refreshLabelList(){
     var regTel = /^\+?(\d|[\(\)]){3,}$/;
     var value = $("form #phone_number").val();
     var destList = [];
-    if (value.trim().length > 1) {
-        var content = '';
+    $('#destnum-label').html('');
+    $('#tagsnum').tagsinput('removeAll')
+    if (value.trim().length > 0) {
         value.split(',').forEach(function(num){
-            if (regTel.test(num.trim()) && destList.indexOf(num.trim()) == -1){
-                content += labelRemovable(num.trim());
-                destList.push(num.trim())
+            num = num.trim();
+            if (regTel.test(num) && destList.indexOf(num) == -1){
+                $('#destnum-label').append(labelRemovable(num));
+                destList.push(num)
             }
         });
-        $('#destnum-label').html(content);
+        $('#destnum-label .label').each(function() {
+            var $this = $(this);
+            if($this.attr('data-telnum') != $this.text().trim()){
+                $('#tagsnum').tagsinput('add', {num:$this.attr('data-telnum') , name: $this.text().trim()});
+            }
+        });
         $("form #phone_number").val(destList.join(','));
     } else {$("form #phone_number").val('');}
     refreshSmsLength();
@@ -116,18 +141,29 @@ function refreshSmsLength(){
 }
 
 function addNum(num, name){
-    refreshLabelList();
-    var value = $("form #phone_number").val();
-    var content = $('#destnum-label').html();
-
-    if (value.trim().length > 1) {
-        $("form #phone_number").val(value +','+num);
-    } else {
-        $("form #phone_number").val(num);
+    if($('#destnum-label [data-telnum="'+num+'"]').length == 0) {
+        var value = $("form #phone_number").val();
+        if (value.trim().length > 1) {
+            $("form #phone_number").val(value +','+num);
+        } else {
+            $("form #phone_number").val(num);
+        }
+        refreshLabelList();
+        refreshSmsLength();        
     }
-    content += labelRemovable(num, name);
-    $('#destnum-label').html(content);
-    refreshSmsLength();
+
+}
+
+function removeNum(num){
+    var nums = $('form #phone_number').val().split(',');
+
+    if (nums.indexOf(num) > -1) {
+        nums.splice(nums.indexOf(num),1);
+        $('form #phone_number').val(nums.join(','));
+        $('#tagsnum').tagsinput('remove', {num:num})
+        refreshLabelList();
+    }
+    
 }
 
 $(document).ready(function() {
@@ -146,16 +182,7 @@ $(document).ready(function() {
     })
 
     $(document).on('click', '.label-tel > .fa', function() {
-        var num = $(this).parent().attr('data-telnum');
-        var nums=$('form #phone_number').val().split(',');
-        var content = '';
-
-        nums.splice(nums.indexOf(num),1);
-        nums.forEach(function(num){content += labelRemovable(num)});
-        $('form #phone_number').val(nums.join(','));
-        $('#destnum-label').html(content);
-        $('#tagsnum').tagsinput('remove', {num:num})
-        refreshSmsLength();
+        removeNum($(this).parent().attr('data-telnum'));
     });
 
     $('form #content').keyup(function(){
