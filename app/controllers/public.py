ff61d78117fb40a4103cccbee2be821f2a5d682f
thivolle cedric <cedric@thivolle-cazat.fr>
2016-04-04 21:58:09 +0200

    ajout de meta data on public site

--- app/controllers/public.py
+++ app/controllers/public.py
@@ -5,25 +5,51 @@ from markdown2 import markdown
 
 PUBLIC = Blueprint('PUBLIC', __name__)
 
+DESC = "Projet public d'application web présentant les possibilités d'intégration de"
+DESC += " l'A.P.I. Voxity. Click2call, notification d'appels, consultation de l'état'"
+DESC += " des postes, envoie de SMS."
+TAGS = [
+    "Voxity",
+    "API",
+    "Application web",
+    "SMS",
+    "Notification d'appels",
+    "etat des postes",
+    "Samples",
+    "Exemple",
+    "vox-ui-api"
+]
+
 
 @PUBLIC.route("")
 @PUBLIC.route("index.html")
 def index():
-    return render_template('public/index.html')
+    return render_template(
+        'public/index.html',
+        meta_desc=DESC,
+        meta_keywords_list=TAGS
+    )
 
 
 @PUBLIC.route('changelog.html', methods=["GET"])
 def changelog():
+    local_tag = TAGS
+    local_tag.append('Évolution')
+    local_tag.append('Mise à jour')
     return render_template(
         'public/changelog.html',
+        meta_keywords_list=TAGS,
         changelog=markdown(render_template('changelog.md')),
         page_title="changelog"
     )
 
 
 @PUBLIC.route('about.html', methods=["GET"])
-def about():
-    return render_template('public/about.html', page_title="à propos")
+def about(): 
+    return render_template(
+        'public/about.html',
+        page_title="à propos"
+    )
 
 
 @PUBLIC.route('features.html', methods=["GET"])
