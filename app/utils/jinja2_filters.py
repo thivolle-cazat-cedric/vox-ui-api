e35d2861ec11696137861b1544c170cf1471c5d6
thivolle cedric <cedric@thivolle-cazat.fr>
2016-05-17 21:40:57 +0200

    improve filter

--- app/utils/jinja2_filters.py
+++ app/utils/jinja2_filters.py
@@ -5,13 +5,15 @@ from jinja2.filters import do_mark_safe
 from flask import session
 
 EXTERNAL_PHONE_NUM_RE = re.compile('\+?\d{5,}')
+PHONE_NUM_FR = re.compile('\d{10}')
 
 
 def number_clear(value, space=True):
+    num = value
     if value and len(value) > 3 and value[0:3] == "+33":
         value = '0' + value[3:]
 
-    if value and space and EXTERNAL_PHONE_NUM_RE.match(value):
+    if value and space and PHONE_NUM_FR.match(value):
         value = " ".join(value[i:i + 2] for i in range(0, len(value), 2))
 
     return value or ''
